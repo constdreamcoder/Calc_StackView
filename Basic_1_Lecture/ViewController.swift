b3f9169d4f65fa4717c844161bd7c8a00f5a965f
constdreamcoder <scdreamricher@gmail.com>
2022-11-19 13:45:09 +0900

    Initial Commit

---
+++ Basic_1_Lecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic_1_Lecture
+//
+//  Created by mac on 2022/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
