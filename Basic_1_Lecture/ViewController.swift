4cba295485cf111e12cd27bfffc96fec9945a028
constdreamcoder <scdreamricher@gmail.com>
2022-11-26 21:17:13 +0900

    Graph

--- Basic_1_Lecture/ViewController.swift
+++ Basic_1_Lecture/ViewController.swift
@@ -7,88 +7,68 @@
 
 import UIKit
 
-class ExpandCell: UITableViewCell {
-    
-    @IBOutlet weak var descriptionLabel: UILabel!
+extension NSLayoutConstraint {
+    func changeMultiplier(value: CGFloat) -> NSLayoutConstraint {
+        
+        NSLayoutConstraint.deactivate([self])
+        
+        let newConstraint = NSLayoutConstraint.init(item: self.firstItem, attribute: self.firstAttribute, relatedBy: self.relation, toItem: self.secondItem, attribute: self.secondAttribute, multiplier: value, constant: self.constant)
+        
+        newConstraint.priority = self.priority
+        newConstraint.shouldBeArchived = self.shouldBeArchived
+        newConstraint.identifier = self.identifier
+        
+        NSLayoutConstraint.activate([newConstraint])
+        
+        return newConstraint
+    }
 }
+
 
 class ViewController: UIViewController {
     
-    // 필요한 텍스트 포함
-    // expand 상태
-    
-    // Model
-    struct ExpandDataModel {
-        var description: String
-        var isExpand: Bool
-    }
-    
-    var dataModels = [ExpandDataModel]()
+    @IBOutlet weak var graph1Height: NSLayoutConstraint!
+    @IBOutlet weak var graph2Height: NSLayoutConstraint!
+    @IBOutlet weak var graph3Height: NSLayoutConstraint!
+    @IBOutlet weak var graph4Height: NSLayoutConstraint!
+    @IBOutlet weak var graph5Height: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let textArray = ["short Text",
-        "long long long long long long long long long long long long long long long long long long long long long long long Text",
-        "long long long long long long long long long long long long long long long long long long long long long long long Text",
-        "long long long long long long long long long long long long long long long long long long long long long long long Text",
-         "long long long long long long long long long long long long long long long long long long long long long long long Text",
-         "long long long long long long long long long long long long long long long long long long long long long long long Text",
-         "long long long long long long long long long long long long long long long long long long long long long long long Text"]
+       
 
-        for (_, value) in textArray.enumerated() {
-            dataModels.append(ExpandDataModel.init(description: value, isExpand: false))
+    }
+    
+    @IBAction func style1(_ sender: UIButton) {
+       
+        
+        UIView.animate(withDuration: 0.5) {
+            
+            self.graph1Height = self.graph1Height.changeMultiplier(value: 0.9)
+            self.graph2Height = self.graph2Height.changeMultiplier(value: 0.8)
+            self.graph3Height = self.graph3Height.changeMultiplier(value: 0.7)
+            self.graph4Height = self.graph4Height.changeMultiplier(value: 0.6)
+            self.graph5Height = self.graph5Height.changeMultiplier(value: 0.5)
+            
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @IBAction func style2(_ sender: UIButton) {
+        
+        UIView.animate(withDuration: 0.5) {
+            
+            self.graph1Height = self.graph1Height.changeMultiplier(value: 0.5)
+            self.graph2Height = self.graph2Height.changeMultiplier(value: 0.6)
+            self.graph3Height = self.graph3Height.changeMultiplier(value: 0.7)
+            self.graph4Height = self.graph4Height.changeMultiplier(value: 0.8)
+            self.graph5Height = self.graph5Height.changeMultiplier(value: 0.9)
+            
+            self.view.layoutIfNeeded()
         }
     }
 }
 
-extension ViewController: UITableViewDataSource, UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataModels.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "expandCell_ID", for: indexPath) as? ExpandCell else {
-            return UITableViewCell()
-        }
-        cell.descriptionLabel.text = dataModels[indexPath.row].description
-        
-        if dataModels[indexPath.row].isExpand == true {
-            cell.descriptionLabel.numberOfLines = 0
-        } else {
-            cell.descriptionLabel.numberOfLines = 1
-        }
-        
-        cell.selectionStyle = .none
-        
-        return cell
-    }
-    
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//
-//    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        dataModels[indexPath.row].isExpand = !dataModels[indexPath.row].isExpand
-        
-        // Performance Improvement
-        
-        // 1. heightForRowAt 에서 정확한 높이를 지정(확실히 해결) -> 퍼포먼스 문제 없음
-    
-        // 2. tableView.reloadData() -> 어느 정도 해결
-        
-        // 3. 어느정도 해결
-        // tableView.estimatedSectionHeaderHeight = 0
-        // tableView.estimatedSectionFooterHeight = 0
-        
-        
-        // 4. 애니메이션 효과 없애는 거의 확실히 해결(View 전체 애니메이션을 없애기)
-        // UIView.setAnimationsEnabled(false)
-        // UIView.setAnimationsEnabled(true)
 
-        tableView.reloadRows(at: [indexPath], with: .none)
-
-    }
-}
 
